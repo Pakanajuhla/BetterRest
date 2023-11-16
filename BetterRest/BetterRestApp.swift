7e0ff32d5ae5fc2e4bba0950101420e7635733ae
Vladyslav <135268924+Pakanajuhla@users.noreply.github.com>
2023-11-16 11:30:33 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Владислав on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
